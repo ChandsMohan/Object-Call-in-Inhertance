e883e6a98038b8393b3957acf633cdc9454af134
MohanSonia <31935268+MohanSonia@users.noreply.github.com>
2018-03-31 13:14:56 +0530

    Object Call

---
+++ Object_Calls In Inheritance/Base Class And Derive Class/Base Class And Derive Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Base_Class_And_Derive_Class
+{
+   public class Program
+    {
+        public void Method1()
+        {
+            string a = "Its a Base method";
+            Console.WriteLine(a);
+        }
+    }
+    public class DerivedClass:Program
+    {
+        public new void Method1()
+        {
+            string a = "Its a Derived Class";
+            Console.WriteLine(a);
+        }
+    }
+    public class ForMain
+    {
+
+        static void Main(string[] args)
+        {
+            DerivedClass DerivedClass_Obj = new DerivedClass();
+            Program BaseClass_Obj = (Program)DerivedClass_Obj;
+            BaseClass_Obj.Method1();
+            //DerivedClass_Obj.Method1();
+            //DerivedClass_Obj.Method2();
+            Console.ReadKey();
+
+        }
+    }
+}
